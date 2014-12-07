62108e062b3b7b633e84152f120ab57b071b617f
yifita <yifanwang2010@gmail.com>
2014-12-07 19:20:06 +0100

    policy iteration works

--- src/ScriptI.m
+++ src/ScriptI.m
@@ -87,23 +87,24 @@ G = ComputeStageCostsI( stateSpace, controlSpace, disturbanceSpace, ...
 % cost can be set to infinity.
 
 %% solve stochastic shortest path problem
-[ J_opt_vi, u_opt_ind_vi ] = ValueIteration( P, G );
-%[ J_opt_pi, u_opt_ind_pi ] = PolicyIteration( P, G );
+%[ J_opt_vi, u_opt_ind_vi ] = ValueIteration( P, G );
+[ J_opt_pi, u_opt_ind_pi ] = PolicyIteration( P, G );
 %[ J_opt_lp, u_opt_ind_lp ] = LinearProgramming( P, G );
 % Here we solve the stochastic shortest path problem by Value Iteration,
 % Policy Iteration, and Linear Programming.
 
 %% plot results
-figH = PlotMaze( 2, mazeSize, walls, targetCell, [], [], stateSpace, ...
-    controlSpace, J_opt_vi, u_opt_ind_vi );
-figure(figH);
-title(strcat('Value iteration (width=', num2str(mazeSize(1)), ', height=', num2str(mazeSize(2)), ')'));
-return;
+%figH = PlotMaze( 2, mazeSize, walls, targetCell, [], [], stateSpace, ...
+%    controlSpace, J_opt_vi, u_opt_ind_vi );
+%figure(figH);
+%title(strcat('Value iteration (width=', num2str(mazeSize(1)), ', height=', num2str(mazeSize(2)), ')'));
+%return;
 
 figH = PlotMaze( 3, mazeSize, walls, targetCell, [], [], stateSpace, ...
     controlSpace, J_opt_pi, u_opt_ind_pi );
 figure(figH);
 title(strcat('Policy iteration (width=', num2str(mazeSize(1)), ', height=', num2str(mazeSize(2)), ')'));
+return;
 
 figH = PlotMaze( 4, mazeSize, walls, targetCell, [], [], stateSpace, ...
     controlSpace, J_opt_lp, u_opt_ind_lp );
