e606fc249cbeee9be37466756c1fb3e94cf3519c
frenaut <felix.renaut@gmail.com>
2014-12-08 17:10:11 +0100

    First try for Part II, there must be some error since it seems to go through walls...

--- src/ComputeTransitionProbabilitiesII.m
+++ src/ComputeTransitionProbabilitiesII.m
@@ -57,5 +57,164 @@ function P = ComputeTransitionProbabilitiesII( stateSpace, controlSpace, disturb
 
 % put your code here
 
+numStates = size(stateSpace,1);
+numInput = size(controlSpace,1);
+width = mazeSize(1);
+height = mazeSize(2);
+numWalls = size(walls,2)/2;
+wallStarts = walls(:,mod(1:numWalls*2,2)==1);
+wallEnds = walls(:,mod(1:numWalls*2,2)==0);
+wallStarts = wallStarts';
+wallEnds = wallEnds';
+P = zeros(numStates,numStates,numInput);
+for k = 1:numStates
+    pos = stateSpace(k,:);
+
+    if isequal(pos, targetCell')
+        P(k, k, :) = zeros(numInput, 1);
+        P(k, k, 7) = 1;
+        continue;
+    end
+
+    for l = 1:numInput
+        control = controlSpace(l,:);
+        pos_new = pos + control;
+
+        % control input results in crossing the borders or hitting a wall
+        if (hitBorder(pos,control) || hitWall(pos,control))
+            continue;
+        end
+		% control input results in falling into Hole
+		if fallIntoHole(pos,control)
+			pos_new = resetCell';
+		end
+        % input not hitting the wall, check whether hit wall after all possible
+        % disturbance is applied
+        for m = 1:size(disturbanceSpace,1)
+            disturbance = disturbanceSpace(m,1:2);
+            prob = disturbanceSpace(m,3);
+
+            % if not hit wall or falls after disturbance is applied
+            if (~hitBorder(pos_new,disturbance) &&...
+                    ~hitWall(pos_new,disturbance) )
+				if ~fallIntoHole(pos_new,disturbance)
+                	pos_disturbed = pos_new + disturbance;
+				else
+					pos_disturbed = resetCell;
+				end
+                nextState = (pos_disturbed(1)-1) * height + pos_disturbed(2);
+                P(k,nextState,l) = P(k,nextState,l)+ prob;
+            else
+                % if hit wall after disturbance is applied, the ball stays at its position 
+                nextState = (pos_new(1)-1) * height + pos_new(2);
+                P(k,nextState,l) = P(k,nextState,l)+ prob;
+            end
+        end
+    end
+
+end
+% check starting from one pos a move would lead to hitting a wall
+function h = hitWall(pos,move)
+    % move = [0,0]
+    if(move(1) == 0 && move(2) ==0)
+        h = false;
+        return
+    end
+    % K'*2 matrix storing relevant walls
+    wallStartToCheck = [];
+    wallEndToCheck = [];
+    % horizontal move
+    if(move(2) == 0)
+        switch move(1)
+            case 1
+                wallStartToCheck = [wallStartToCheck;pos + [0,-1]];
+                wallEndToCheck = [wallEndToCheck;pos];
+            case 2
+                wallStartToCheck = [wallStartToCheck;pos + [0,-1]];
+                wallEndToCheck = [wallEndToCheck;pos];
+                wallStartToCheck = [wallStartToCheck;pos + [1,-1]];
+                wallEndToCheck = [wallEndToCheck;pos + [1,0]];
+            case -1
+                wallStartToCheck = [wallStartToCheck;pos + [-1,-1]];
+                wallEndToCheck = [wallEndToCheck;pos + [-1,0]];
+            case -2
+                wallStartToCheck = [wallStartToCheck;pos + [-1,-1]];
+                wallEndToCheck = [wallEndToCheck;pos + [-1,0]];
+                wallStartToCheck = [wallStartToCheck;pos + [-2,-1]];
+                wallEndToCheck = [wallEndToCheck;pos + [-2,0]];
+        end
+    elseif(move(1)==0)
+        % vertical move
+        switch move(2)
+            case 1
+                wallStartToCheck = [wallStartToCheck;pos + [-1,0]];
+                wallEndToCheck = [wallEndToCheck;pos];
+            case 2
+                wallStartToCheck = [wallStartToCheck;pos + [-1,0]];
+                wallEndToCheck = [wallEndToCheck;pos];
+                wallStartToCheck = [wallStartToCheck;pos + [-1,1]];
+                wallEndToCheck = [wallEndToCheck;pos + [0,1]];
+            case -1
+                wallStartToCheck = [wallStartToCheck;pos + [-1,-1]];
+                wallEndToCheck = [wallEndToCheck;pos + [0,-1]];
+            case -2
+                wallStartToCheck = [wallStartToCheck;pos + [-1,-1]];
+                wallEndToCheck = [wallEndToCheck;pos + [0,-1]];
+                wallStartToCheck = [wallStartToCheck;pos + [-1,-2]];
+                wallEndToCheck = [wallEndToCheck;pos + [0,-2]];
+        end
+    else
+        % diagonal move just need to check whether interested point is in
+        % wall matrix
+        pointToCheck = [];
+        if (isequal(move, [1,1]))
+            pointToCheck = pos;
+        elseif (isequal(move, [1,-1]))
+            pointToCheck = pos + [0,-1];
+        elseif (isequal(move, [-1,-1]))
+            pointToCheck = pos + [-1,-1];
+        elseif (isequal(move, [-1,1]))
+            pointToCheck = pos + [-1,0];
+        end
+        h = (sum(ismember(walls',pointToCheck,'rows'))>0);
+        return
+    end
+    % check whether elements in wallToCheck is in walls
+    % convert to K*2 matrix
+    for i = 1:size(wallStartToCheck,1)
+        index = ismember(wallStarts,wallStartToCheck(i,:),'rows');
+        if sum(index) == 0
+            continue;
+        end
+        found = ismember(wallEnds(index,:),wallEndToCheck(i,:),'rows');
+        if sum(found) > 0
+            h = true;
+            return;
+        end
+    end
+    h = false;
+end
+% check starting from one pos a move would lead to going out of border
+function h = hitBorder(pos,move)
+    pos = pos + move;
+    if (pos(1)<1 ||pos(1)>width ||...
+                pos(2)<1||pos(2)>height)
+            h = true;
+            return
+    end
+    h = false;
+end
+
+% check starting from one pos a move would lead to falling into a hole
+function h = fallIntoHole(pos,move)
+	pos = pos + move;
+	for i=1:size(holes,2)
+		if isequal(pos',holes(:,i))
+			h=true;
+			return;
+		end
+	end
+	h=false;
+end
 end
 
