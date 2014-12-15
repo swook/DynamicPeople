7645a99d2b65ab5d8a592843b80516acdfd6c07f
yifita <yifanwang2010@gmail.com>
2014-12-15 20:27:30 +0100

    linear programming works

--- src/LinearProgramming.m
+++ src/LinearProgramming.m
@@ -31,6 +31,45 @@ function [ J_opt, u_opt_ind ] = LinearProgramming( P, G )
 %       	inputs for each element of the state space.
 
 % put your code here
-
+[MN,L] = size(G);
+f = -1 * ones(MN-1,1);
+% find target cell
+[target_ind,~] = find(G==0);
+% get all non-target states
+iter_states = 1:MN;
+iter_states = iter_states( iter_states~=target_ind);
+% (MN-1) x L constraints
+% p: a ((MN-1) x L) x (MN-1) matrix. Pij(u) is stored in p((u-1)*(MN-1) + i,j)
+% g: a ((MN-1) x L) x 1 vector. g(i,u) is stored in g((u-1)*(MN-1) +i)
+A = [];
+g = [];
+for u = 1:L
+    for i = 1: numel(iter_states)
+        % ignore infeasible input control
+        if G(iter_states(i),u) == Inf
+            continue;
+        end
+        p = P(iter_states(i),iter_states,u);
+        g(end+1) = G(iter_states(i),u);
+        curr_row = zeros(1,MN-1);
+        curr_row(i) = 1;
+        A(end+1,:) = curr_row - p;
+    end
+end
+J_opt = linprog(f,A,g);
+J_opt = J_opt';
+% must consider termination stage in maximization stage
+J_opt(iter_states) = J_opt;
+J_opt(target_ind) = 0;
+% minimization stage
+J_candidates = zeros(L, MN);
+for l = 1:L
+    % J_candidates(u, :): 1  x MN
+    % P(:, :, u):         MN x MN
+    % J_k:                1  x MN
+    J_candidates(l, :) = J_opt * P(:, :, l)';
+end
+J_candidates = J_candidates + G';
+[~, u_opt_ind] = min(J_candidates);
 end
 
